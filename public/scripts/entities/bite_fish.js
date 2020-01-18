1f22a1caf61cfa88a26aaee19216b7e54a8c5682
Noah Dumba <noahdumba1@gmail.com>
2020-01-18 00:15:31 +0000

    added the biteFish and gave him goFish properties

---
+++ public/scripts/entities/bite_fish.js
@@ -0,0 +1,7 @@
+class BiteFish extends GoFish {
+
+  constructor(options) {
+    super(options); // Call super to run the code inside `Fish`'s constructor
+    this.imageUri = '/images/bitefish.png'; // Set the image
+  }
+}
